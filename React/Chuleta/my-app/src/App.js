90668a30cef50e1ff778df096be848dd92f72ea4
Antonio Muñoz <antonio.bjjrga@gmail.com>
2021-04-19 13:55:48 +0200

    react

---
+++ React/Chuleta/my-app/src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import FirstComponet from './componets/FirstComponet';
+
+function App() {
+  return (
+    <div className="App">
+      <FirstComponet/>
+    </div>
+  );
+}
+
+export default App;
---
+++ React/Chuleta/my-app/src/componets/FirstComponet.js
@@ -0,0 +1,10 @@
+export default function FirstComponet() {
+    return (
+        <div>
+            <h3>Titulo</h3>
+            <small>16 de Agosto de 2025</small>
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus accusamus quisquam earum rerum similique optio deserunt.</p>
+        </div>
+    )
+}
+//export
