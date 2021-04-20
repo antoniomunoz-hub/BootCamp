370cfc59eea1e57d3d777f1b4bfc88d104ca91c8
Antonio Muñoz <antonio.bjjrga@gmail.com>
2021-04-20 13:56:46 +0200

    ejercicios React

--- React/Chuleta/my-app/src/App.js
+++ React/Chuleta/my-app/src/App.js
@@ -1,11 +1,14 @@
 
 import './App.css';
-import FirstComponet from './componets/FirstComponet';
-
+import FirstComponet, {SecondComponent, ThirdComponent} from './componets/FirstComponet';
+//Si tenemos otro componente en otro archivo necesitamos crear otra linea con la ruta del archivo
 function App() {
   return (
     <div className="App">
-      <FirstComponet/>
+      <FirstComponet title ="Titulo enviado desde el padre" date="16 Agosto 2021"/>
+      <FirstComponet title ="Otro Titulo" date="Otra Fecha"/>
+      <SecondComponent/>
+      <ThirdComponent/>
     </div>
   );
 }
--- React/Chuleta/my-app/src/componets/FirstComponet.js
+++ React/Chuleta/my-app/src/componets/FirstComponet.js
@@ -1,11 +1,36 @@
-export default function FirstComponet() {
+import  {Fragment} from "react"; // Si utulizamos forma abreviada no es necesario el import 
+export default function FirstComponet(props) {
+    console.log(props, typeof props);
+    console.log(props.title);
+    
     return (
-        <div>
-            <h3>Titulo</h3>
-            <small>16 de Agosto de 2025</small>
+        <Fragment>
+            <h3>{props.title}</h3> 
+            <small>{props.date}</small>
             <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus accusamus quisquam earum rerum similique optio deserunt.</p>
-        </div>
+        </Fragment>
     )
 }
 
 //Export default FirstComponent: // Alternativa
+//Solo se puede un export default por archivo
+
+function SecondComponent() {
+    return (
+        <>
+            <h2>Soy Otro Componente</h2>
+        </>
+    )
+
+}// Forma abreviada de usar Fragment
+
+
+function ThirdComponent() {
+    return(
+        <div>
+            <h2>Un tercer Componente</h2>
+        </div>
+    )
+}
+
+export {SecondComponent, ThirdComponent};
