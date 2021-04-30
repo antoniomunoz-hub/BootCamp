4e9d2b0908ec841b67bb43390c32fdb7acbbeb62
Antonio Muñoz <antonio.bjjrga@gmail.com>
2021-04-30 13:17:34 +0200

    ejercicio final seguimiento

--- React/Ejercicios/ejercicio7/src/Pages/LastsMovies.js
+++ React/Ejercicios/ejercicio7/src/Pages/LastsMovies.js
@@ -3,12 +3,12 @@ import {Card, Button} from "react-bootstrap";
 
 export default function LastsMovies() {
     
-    const[movie, setMovie] = useState([]);
+    const[movie, setMovie] = useState({});
     useEffect(() => {
       
-      fetch('https://api.themoviedb.org/3/movie/upcoming?api_key=64630dedf75d27e2299520a51e45e9b1 ')
+      fetch('https://api.themoviedb.org/3/movie/550?api_key=64630dedf75d27e2299520a51e45e9b1 ')
       .then(response => response.json())
-      .then(data => (setMovie(data.results)));
+      .then(data => (setMovie(data)));
      
     
     }, [])
