845fea6c91b72d3d6ff27e090aaa3419b910da8c
Antonio Muñoz <antonio.bjjrga@gmail.com>
2021-05-03 14:01:03 +0200

    primer dia base datos

--- React/Ejercicios/ejercicio7/src/Pages/Details.js
+++ React/Ejercicios/ejercicio7/src/Pages/Details.js
@@ -1,10 +1,11 @@
 
 import React,{useEffect, useState} from "react";
+import {useParams} from "react-router-dom";
 
-
-export default function Details({id}) {
+export default function Details() {
     
     const[movie, setMovie] = useState({});
+    const {id} = useParams();
     useEffect(() => {
       
       fetch('https://api.themoviedb.org/3/movie/'+ id +'?api_key=64630dedf75d27e2299520a51e45e9b1')
