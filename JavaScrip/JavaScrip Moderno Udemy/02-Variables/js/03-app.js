5367aab3e4a2a885452382ada2a9f10c1e9f7e31
antoniomunoz-hub <antonio.bjjrga@gmail.com>
2021-08-07 17:36:13 +0200

    js moderno udemy

---
+++ JavaScrip/JavaScrip Moderno Udemy/02-Variables/js/03-app.js
@@ -0,0 +1,5 @@
+const teclado = 'Rii';
+
+// Cuando usamos const NO SE PUEDE REASIGNAR EL VALOR DE LA VARIABLE
+// Tienes que inizializarla con un valor no puedes asiganrla sin valor para darlo posteriormente
+//
---
+++ JavaScrip/JavaScrip Moderno Udemy/03-Strings/js/02-app.js
@@ -0,0 +1,4 @@
+const hijo = 'Manuel';
+
+console.log(hijo);
+console.log(hijo.lenght);
