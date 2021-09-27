0cf3e26a2f54122db2be183b3f6438ffdb81170a
antoniomunoz-hub <antonio.bjjrga@gmail.com>
2021-09-27 15:56:14 +0200

    filtrado

--- JavaScript/JavaScript Moderno Udemy/Curso JS Moderno/17-PROYECTO-Buscador/js/app.js
+++ JavaScript/JavaScript Moderno Udemy/Curso JS Moderno/17-PROYECTO-Buscador/js/app.js
@@ -24,13 +24,45 @@ const datosBusqueda = {
 
 }
 // Eventos para select de busqueda
-marca.addEventListener()
 
+marca.addEventListener('change', e => {
+    datosBusqueda.marca = e.target.value;    
+    filtrarAuto();
+});
+
+year.addEventListener('change', e => {
+    datosBusqueda.year = e.target.value;  
+    filtrarAuto();
+  
+});
+
+minimo.addEventListener('change', e => {
+    datosBusqueda.minimo = e.target.value;    
+    filtrarAuto();
+});
+
+maximo.addEventListener('change', e => {
+    datosBusqueda.maximo = e.target.value;  
+    filtrarAuto()  
+});
+
+puertas.addEventListener('change', e => {
+    datosBusqueda.puertas = e.target.value;    
+});
+
+color.addEventListener('change', e => {
+    datosBusqueda.color = e.target.value;    
+});
+
+transmision.addEventListener('change', e => {
+    datosBusqueda.transmision = e.target.value;    
+    console.log(datosBusqueda);
+});
 
 //Eventos
 
 document.addEventListener('DOMContentLoaded', () => {
-    mostrarAutos();
+    mostrarAutos(autos);
 
     //Llena las opciones
     llenarSelect();    
@@ -39,7 +71,10 @@ document.addEventListener('DOMContentLoaded', () => {
 
 //Funciones
 
-function mostrarAutos(){
+function mostrarAutos(autos){
+
+    limpiarHTML();// Elimina el html previo
+
     autos.forEach( auto => {
         
         const { marca, modelo, year, puertas, transmision, precio, color } = auto;
@@ -54,6 +89,59 @@ function mostrarAutos(){
     });
 }
 
+// Limpiar HTML
+
+function limpiarHTML(){
+    while(resultado.firstChild){
+        resultado.removeChild(resultado.firstChild);
+    }
+
+}
+
+//Funcion que filtra en base a la busqueda
+
+function filtrarAuto(){
+    const resultado = autos.filter(filtrarMarca).filter(filtrarYear).filter(filtrarMinimo).filter(filtrarMaximo)
+
+    // console.log(resultado);
+    mostrarAutos(resultado);
+}
+
+function filtrarMarca(auto){
+    const {marca} = datosBusqueda;
+
+    if(marca){
+        return auto.marca === marca;
+    }
+        return auto;
+}
+
+function filtrarYear(auto){
+    const {year} = datosBusqueda;
+
+    if(year){
+        return auto.year === parseInt(year); // Parseamos y convertimos a entero el year ya que los datos que recibimos por el form es un string
+    }
+        return auto;    
+}
+
+function filtrarMinimo(auto){
+    const {minimo} = datosBusqueda;
+
+    if(minimo){
+        return auto.precio >= minimo;
+    }
+        return auto;
+}
+
+function filtrarMaximo(auto){
+    const {maximo} = datosBusqueda;
+
+    if(maximo){
+        return auto.precio <= maximo;
+    }
+        return auto;
+}
 //Genera los años del Select
 
 function llenarSelect(){
