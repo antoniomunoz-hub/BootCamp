852bac3ef47c8bf9f9c714ade88cff4eee05803a
antoniomunoz-hub <antonio.bjjrga@gmail.com>
2021-10-18 16:06:49 +0200

    creando carro de compra

---
+++ React/carro-de-compras/src/App.js
@@ -0,0 +1,27 @@
+import { Component } from 'react';
+import Productos from './components/Productos'
+
+
+
+class App extends Component {
+    state = {
+      productos: [
+        {name: 'Tomate', price: '3', img:'/productos/tomate.jpg'},
+        {name: 'Arbejas', price: '4', img:'/productos/arbejas.jpg'},        
+        {name: 'Lechuga', price: '2', img:'/productos/lechuga.jpg'} 
+      ]
+    }
+    
+    render(){
+    return(
+      <div>
+        <Productos
+        agregarAlCarro = {()=> console.log('De momento no hace nada')}
+        productos = {this.state.productos}
+        />
+      </div>
+    )
+  }
+}
+
+export default App;
