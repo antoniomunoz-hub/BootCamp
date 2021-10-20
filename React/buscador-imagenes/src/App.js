057744f1ee3c105692e02d1055465ef2e19ac493
antoniomunoz-hub <antonio.bjjrga@gmail.com>
2021-10-20 10:59:09 +0200

    buscador de imaganes apiunsplash

---
+++ React/buscador-imagenes/src/App.js
@@ -0,0 +1,49 @@
+import {Formik, Form, Field} from 'formik'
+import { useState } from 'react'
+import './styles/header.css'
+import './styles/content.css'
+import './styles/article.css'
+
+
+
+const App = () => {
+  const [photos, setPhotos] = useState([])
+  const open = url => window.open(url)
+  console.log({photos});
+    return (
+      <div>
+        <header>
+          <Formik
+            initialValues = {{search: ''}}
+            onSubmit= {async values => {
+              const response = await fetch(
+                `https://api.unsplash.com/search/photos?per_page=20&query=${values.search}`,{
+                headers:{
+                  'Authorization': 'Client-ID -B8ejaYgEteMSfd_qyLAbqe89D34_3RoiVMxrDee9_U'}
+                }
+              )
+              const data = await response.json()
+              //llamada a la api unsplash
+              setPhotos(data.results)
+            }}
+          >
+            <Form>
+              <Field name = "search"/>
+            </Form>
+          </Formik>
+        </header>
+        <div className="container">
+          <div className="center">
+            {photos.map(photo =>
+              <article key={photo.id} onClick={() => open(photo.links.html)}>
+                <img src = {photo.urls.regular}/>
+                <p>{[photo.description, photo.alt_description].join ('-')}</p>
+              </article>)}
+        </div>
+      </div>
+    </div>
+  )
+}
+
+
+export default App
