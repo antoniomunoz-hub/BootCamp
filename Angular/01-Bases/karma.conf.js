ebb56aad966ead1940fff17d9a42e106588f1cee
antoniomunoz-hub <antonio.bjjrga@gmail.com>
2021-12-25 20:42:02 +0100

    retomamos angular

--- Angular/01-Bases/karma.conf.js
+++ Angular/01-Bases/karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/bases'),
+      dir: require('path').join(__dirname, './coverage/Bbases'),
       subdir: '.',
       reporters: [
         { type: 'html' },
